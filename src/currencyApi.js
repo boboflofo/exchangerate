1adf1910107bfc312af0700fad53f1da1c01c09c
Jonathan Song <jonats10@uci.edu>
2023-11-17 10:01:46 -0800

    add try catch logic

--- src/currencyApi.js
+++ src/currencyApi.js
@@ -1,6 +1,12 @@
 class Currency {
   static async getExchange() {
-    const response = await fetch(`https://v6.exchangerate-api.com/v6/YOUR-API-KEY/pair/EUR/GBP`)
+    try {
+      const response = await fetch(
+        `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${base}/${target}`
+      );
+      const responseJson = await response.json();
+    } catch (error) {
+      return error;
+    }
   }
 }
-
