e3d7248b3be4bdf5a422481b4d0c3ff03afeb88d
Jonathan Song <jonats10@uci.edu>
2023-11-17 14:22:47 -0800

    WIP env file recognition

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import "./css/styles.css";
 import Currency from "./currencyApi";
 import convert from "./currencyConversion";
 
-function convertCurrency(base, target) {
-  const response = Currency.getExchange(base, target);
+async function convertCurrency(base, target) {
+  const response = await Currency.getExchange(base, target);
   if (response.error) {
     printError(response);
   } else {
@@ -12,14 +12,16 @@ function convertCurrency(base, target) {
 }
 
 function printElements(base, target, response) {
-  const amount = document.querySelector("#number");
+  const amount = document.querySelector("#number").value;
+  console.log(response)
   const rate = response.conversion_rate;
+  
   const convertedRate = convert(amount, rate);
   document.querySelector(
     ".original"
   ).innerHTML = `Amount in ${base}: ${amount}`;
   document.querySelector(
-    "converted"
+    ".converted"
   ).innerHTML = `Amount in ${target}: ${convertedRate}`;
 }
 
