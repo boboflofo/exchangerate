a255bd4303da5e56101c18dc3d2ba65f52bc7863
Jonathan Song <jonats10@uci.edu>
2023-11-17 12:26:01 -0800

    add error handling for json response

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import "./css/styles.css";
 import Currency from "./currencyApi"
 
 function converyCurrency() {
-  const response = Currency.getExchange(base,target)
+  const response = Currency.getExchange(base,target);
+  if (response.main) {
+    printElements(base,target,response);
+  } else {
+    printError(base,target,response)
+  }
 }
